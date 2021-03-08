a07fd24dd4f644f8ebf28d27415ffac46089ec73
ZongSingHuang <58209813+ZongSingHuang@users.noreply.github.com>
2021-03-08 17:03:02 +0800

    2021.03.08

---
+++ MSEWOA2.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 14:14:14 2020
+
+@author: ZongSing_NB
+
+Main reference:http://www.alimirjalili.com/WOA.html
+Main reference:https://www.sciencedirect.com/science/article/pii/S1568494619307185
+Main reference:https://www.mdpi.com/2076-3417/10/11/3667
+Main reference:http://www.ejournal.org.cn/EN/abstract/abstract11643.shtml#
+"""
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+class MSEWOA():
+    def __init__(self, fit_func, num_dim=30, num_particle=20, max_iter=500, 
+                 b=1, x_max=1, x_min=0, a_max=2, a_min=0, l_max=1, l_min=-1, a2_max=-1, a2_min=-2):
+        self.fit_func = fit_func
+        self.num_dim = num_dim
+        self.num_particle = num_particle
+        self.max_iter = max_iter
+        self.x_max = x_max
+        self.x_min = x_min
+        self.a_max = a_max
+        self.a_min = a_min
+        self.a2_max = a2_max
+        self.a2_min = a2_min
+        self.l_max = l_max
+        self.l_min = l_min
+        self.b = b
+        
+        self._iter = 1
+        self.gBest_X = None
+        self.gBest_score = np.inf
+        self.gBest_curve = np.zeros(self.max_iter)
+        self.chaotic()
+        
+        self.score = self.obl()
+        
+        self.gBest_score = self.score.min().copy()
+        self.gBest_X = self.X[self.score.argmin()].copy()
+        self.gBest_curve[0] = self.gBest_score.copy()
+           
+    def opt(self):
+        self.bound_max = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_max[np.newaxis, :])
+        self.bound_min = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_min[np.newaxis, :])
+        
+        while(self._iter<self.max_iter):
+            mutation = self.levy_flight(self.X[self.score.argmin(), :])
+            if self.fit_func(mutation)<self.gBest_score:
+                self.gBest_X = mutation.copy()
+                self.gBest_score = self.fit_func(mutation)
+                self.gBest_curve[self._iter] = self.gBest_score.copy()
+                self.X[self.score.argmin(), :] = mutation.copy()
+            elif self.fit_func(mutation)<self.score.max():
+                self.X[self.score.argmax(), :] = mutation.copy()
+            
+            a2 = self.a2_max - (self.a2_max-self.a2_min)*(self._iter/self.max_iter)
+            for i in range(self.num_particle):
+                p = np.random.uniform()
+                R2 = np.random.uniform()
+                C = 2*R2
+                l = (a2-1)*np.random.uniform() + 1
+                self.b = np.random.randint(low=0, high=500)
+                                
+                if p>=0.5:
+                    D = np.abs(self.gBest_X - self.X[i, :])
+                    self.X[i, :] = D*np.exp(self.b*l)*np.cos(2*np.pi*l)+self.gBest_X
+                else:
+                    D = C*self.gBest_X - self.X[i, :]
+                    self.X[i, :] = self.gBest_X - D*np.cos(2*np.pi*l)
+
+            self.handle_bound()
+            
+            self.score = self.obl()
+            
+            if np.min(self.score) < self.gBest_score:
+                self.gBest_X = self.X[self.score.argmin()].copy()
+                self.gBest_score = self.score.min().copy()
+            self.gBest_curve[self._iter] = self.gBest_score.copy()
+            self._iter = self._iter + 1
+        
+    def plot_curve(self):
+        plt.figure()
+        plt.title('loss curve ['+str(round(self.gBest_curve[-1], 3))+']')
+        plt.plot(self.gBest_curve, label='loss')
+        plt.grid()
+        plt.legend()
+        plt.show()
+    
+    def levy_flight(self, worst_agent):
+        num = 1
+        idx = np.random.randint(len(worst_agent), size=num)
+        beta = 1.5
+        sigma_v = 1
+        sigma_u = ( ( math.gamma(1+beta)*np.sin(np.pi*beta/2) ) / ( beta*math.gamma((1+beta)/2)*2**((beta-1)/2) ) )**(1/beta)
+        
+        u = np.random.normal(0, sigma_u**2, size=num)
+        v = np.random.normal(0, sigma_v**2, size=num)
+        step = u/(np.abs(v)**(1/beta))
+        
+        new_agent = worst_agent.copy()
+        new_agent[idx] = worst_agent[idx] + step
+        
+        return new_agent
+        
+    def obl(self):
+        k = np.random.uniform() # 不要size
+        alpha = self.X.min(axis=0)
+        beta = self.X.max(axis=0)
+        if len(np.where(np.abs(alpha)==np.inf)[0])>0 or len(np.where(np.abs(beta)==np.inf)[0])>0:
+            loc1 = np.where(np.abs(alpha)==np.inf)
+            loc2 = np.where(np.abs(alpha)==np.inf)
+            alpha[loc1] = np.max(alpha)
+            beta[loc2] = np.min(beta)
+            print('error obl!!!')
+        new_X = k*(alpha+beta)-self.X
+
+        idx_too_low = new_X < self.x_min
+        idx_too_high = new_X > self.x_max
+        
+        rand_X = np.random.uniform(low=alpha, high=beta, size=[self.num_particle, self.num_dim])
+        new_X[idx_too_high] = rand_X[idx_too_high].copy()
+        new_X[idx_too_low] = rand_X[idx_too_low].copy()
+        
+        self.X = np.concatenate((new_X, self.X), axis=0)
+        score = self.fit_func(self.X)
+        top_k = score.argsort()[:self.num_particle]
+        score = score[top_k].copy()
+        self.X = self.X[top_k].copy()
+        
+        return score
+    
+    def handle_bound(self):
+        # (15)
+        R5 = np.random.uniform()
+        R6 = np.random.uniform()
+        idx_too_high = self.x_max < self.X
+        idx_too_low = self.x_min > self.X
+        
+        bound_max_map = self.bound_max[idx_too_high] + \
+                        R5*self.bound_max[idx_too_high]*(self.bound_max[idx_too_high]-self.X[idx_too_high])/self.X[idx_too_high]
+        bound_min_map = self.bound_min[idx_too_low] + \
+                        R6*np.abs(self.bound_min[idx_too_low]*(self.bound_min[idx_too_low]-self.X[idx_too_low])/self.X[idx_too_low])
+
+        self.X[idx_too_high] = bound_max_map.copy()
+        self.X[idx_too_low] = bound_min_map.copy()
+
+        idx_too_high = self.x_max < self.X
+        idx_too_low = self.x_min > self.X
+
+    def chaotic(self):
+            # # Chebyshev (12) Shit
+            # init_X = np.random.uniform(low=-1.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = np.cos((i+1)*np.arccos(init_X))
+            # self.X = (self.X+1) / 2
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Circle (9)
+            # a = 0.5
+            # b = 0.2
+            # init_X = np.random.uniform(low=0.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = np.mod(init_X+b-(a/(2*np.pi))*np.sin(2*np.pi*init_X), 1)
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Gauss/mouse (25) Shit
+            # init_X = np.random.uniform(low=0.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     flag1 = init_X==0
+            #     flag2 = init_X!=0
+            #     init_X[flag1] = 1
+            #     init_X[flag2] = np.mod(1/init_X[flag2], 1)
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Iterative (13)
+            # a = 0.7
+            # init_X = np.random.uniform(low=-1.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = np.sin(a*np.pi/init_X)
+            # self.X = (self.X+1) / 2
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Logistic (5) No3
+            # a = 4
+            # init_X = np.random.uniform(low=0.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = a*init_X*(1-init_X)
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Piecewise (23) Shit
+            # P = 0.4
+            # init_X = np.random.uniform(low=0.0, high=1.0, size=[self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     for j in range(init_X.shape[0]):
+            #         if 0<=init_X[j] and init_X[j]<P:
+            #             init_X[j] = init_X[j]/P
+            #         elif P<=init_X[j] and init_X[j]<0.5:
+            #             init_X[j] = (init_X[j]-P)/(0.5-P)
+            #         elif 0.5<=init_X[j] and init_X[j]<1-P:
+            #             init_X[j] = (1-P-init_X[j])/(0.5-P)
+            #         elif 1-P<=init_X[j] and init_X[j]<1:
+            #             init_X[j] = (1-init_X[j])/P
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Sine (13)
+            # a = 4
+            # init_X = np.random.uniform(low=0.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = (a/4)*np.sin(np.pi*init_X)
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Singer (17) Shit
+            # u = 1.07
+            # while(True):
+            #     init_X = np.random.uniform(low=0.0, high=1.0, size=[1, self.num_dim])
+            #     if len(np.where(init_X>0.999)[0])==0:
+            #         break
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = u*(7.86*init_X-23.31*init_X**2+28.75*init_X**3-13.302875*init_X**4)
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Sinusoidal (13)
+            # a = 2.3
+            # init_X = np.random.uniform(low=0.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = a*init_X**2 * np.sin(np.pi*init_X)
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # Tent (3) No1
+            init_X = np.random.uniform(low=0.0, high=1.0, size=[1, self.num_dim])
+            self.X = np.zeros((self.num_particle, self.num_dim))
+            for i in range(self.num_particle):
+                self.X[i] = init_X
+                bigger = init_X>=0.7
+                smaller = init_X<0.7
+                init_X[bigger] = (10/3)*(1-init_X[bigger])
+                init_X[smaller] = init_X[smaller]/0.7
+            self.X = self.X*(self.x_max-self.x_min) + self.x_min
+    
+            # # Paper (16)
+            # init_X = np.random.uniform(low=-1.0, high=1.0, size=[1, self.num_dim])
+            # self.X = np.zeros((self.num_particle, self.num_dim))
+            # for i in range(self.num_particle):
+            #     self.X[i] = init_X
+            #     init_X = 1 - 2*( np.cos( 4*np.arccos(init_X) ) )**2 # (8)
+            # self.X = (self.X+1) / 2
+            # self.X = self.X*(self.x_max-self.x_min) + self.x_min
+            
+            # # Random (4) No 2
+            # self.X = np.random.uniform(low=self.x_min, high=self.x_max, size=[self.num_particle, self.num_dim])
